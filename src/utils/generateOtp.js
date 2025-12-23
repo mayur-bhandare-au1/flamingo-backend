fe29547d28961823842cc7295c055c55b7af4ada
Mayur Bhandare <mayur.bhandare5@gmail.com>
2025-12-23 17:41:14 +0530

    DEMO OTP ADDED

--- src/utils/generateOtp.js
+++ src/utils/generateOtp.js
@@ -1,8 +1,8 @@
 export const generateOtp = (length = 6) => {
-  let otp = '';
-  for (let i = 0; i < length; i += 1) {
-    otp += Math.floor(Math.random() * 10).toString();
-  }
+  let otp = '123456';
+  // for (let i = 0; i < length; i += 1) {
+  //   otp += Math.floor(Math.random() * 10).toString();
+  // }
   return otp;
 };
 
